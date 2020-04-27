1dd4365d9479bf8a61a57cd7b620e0f2debd528d
Tóth Zoltán <tothzoltan01181996@gmail.com>
2020-04-27 22:15:28 +0200

    Initial Commit

---
+++ szakdolgozat2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  szakdolgozat2020
+//
+//  Created by Tóth Zoltán on 2020. 04. 27..
+//  Copyright © 2020. Tóth Zoltán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
