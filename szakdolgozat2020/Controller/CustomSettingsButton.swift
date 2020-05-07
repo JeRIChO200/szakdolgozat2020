5cd73c79c0d57ed8635d00336239175f2d966386
Tóth Zoltán <tothzoltan01181996@gmail.com>
2020-05-07 17:41:12 +0200

    Settings, CustomSettingsButton class added + Tests foler added

---
+++ szakdolgozat2020/Controller/CustomSettingsButton.swift
@@ -0,0 +1,9 @@
+//
+//  CustomSettingsButton.swift
+//  szakdolgozat2020
+//
+//  Created by Tóth Zoltán on 2020. 05. 07..
+//  Copyright © 2020. Tóth Zoltán. All rights reserved.
+//
+
+import Foundation
---
+++ szakdolgozat2020Tests/szakdolgozat2020Tests.swift
@@ -0,0 +1,33 @@
+//
+//  szakdolgozat2020Tests.swift
+//  szakdolgozat2020Tests
+//
+//  Created by Tóth Zoltán on 2020. 04. 28..
+//  Copyright © 2020. Tóth Zoltán. All rights reserved.
+//
+
+import XCTest
+
+class szakdolgozat2020Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
